c7fb7772314268acf630f43f04a5e0f1d7b60115
Unknown <gabriel.amice.1@ulaval.com>
2017-11-16 00:11:04 -0500

    Added the Controller's skeleton
    Added prototypes of the Controller's functions and some private members.

--- src/Gaudrophone/src/Manager/GaudrophoneController.java
+++ src/Gaudrophone/src/Manager/GaudrophoneController.java
@@ -27,6 +27,136 @@ package Manager;
  *
  * @author frigon
  */
+
+import Music.SoundService;
+import Music.Envelope;
+//import Music.Note;
+import Instrument.Key;
+import KeyUtils.KeyShapeGenerator;
+import KeyUtils.Vector2;
+
 public class GaudrophoneController {
+    private CanvasManager canvas;
+    private SoundService soundService;
     
+    public GaudrophoneController() {
+        canvas = new CanvasManager();
+        soundService = new SoundService();
+    }
+    
+    public void createKey(KeyShapeGenerator key, Vector2 position) {
+        
+    }
+    
+    public void duplicateKey() {
+        
+    }
+    
+    public void deleteKey() {
+        
+    }
+    
+    public void moveKey(Vector2 translation) {
+        
+    }
+    
+    public void resizeKey(Vector2 delta) {
+        
+    }
+    
+    public void pressKey(Key key) {
+        
+    }
+    
+    public void releaseKey(Key key) {
+        
+    }
+    
+    public void createPoint() {
+        
+    }
+    
+    public void deletePoint() {
+        
+    }
+    
+    public void movePoint(Vector2 oldMousePosition, Vector2 newMousePosition) {
+        
+    }
+    
+    public void curveLine(Vector2 oldMousePosition, Vector2 newMousePosition) {
+        
+    }
+    
+    public void setKeyColor(/*Color newColor*/) {
+        
+    }
+    
+    public void setKeySunkenColor(/*Color newColor*/) {
+    
+    }
+    
+    public void setKeyImage(String path) {
+        
+    }
+    
+    public void setKeySunkenImage(String path) {
+        
+    }
+    
+    public void setLineColor(/*Color newColor*/) {
+        
+    }
+    
+    public void setLineThickness(int newThickness) {
+        
+    }
+    
+    public void setSoundName(String newName) {
+        
+    }
+    
+    public void setAudioClip(String path) {
+        
+    }
+    
+    public void removeAudioClip() {
+        
+    }
+    
+    public void setAudioClipReadSpeed(double newSpeed) {
+        
+    }
+    
+    public void setDisplayNote(/*Note newNote, Alteration newAlteration*/) {
+        
+    }
+    
+    public void setDisplayOctave(int newOctave) {
+        
+    }
+    
+    public void setTuning (double newTuning) {
+        
+    }
+    
+    public void setAttack (double newAttack) {
+        
+    }
+        
+    public void setDecay (double newDecay) {
+        
+    }
+        
+    public void setSustain (double newSustain) {
+        
+    }
+        
+    public void setRelease (double newRelease) {
+        
+    }
+        
+    public void setVolume (double newVolume) {
+        
+    }
 }
